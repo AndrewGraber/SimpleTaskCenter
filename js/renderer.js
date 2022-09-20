d8badd15a00a951e506122beb73629c8f96bf557
AndrewGraber <andrewcgraber@gmail.com>
2022-09-20 18:21:07 -0400

    Working on basic window layout

--- js/renderer.js
+++ js/renderer.js
@@ -1,6 +1,32 @@
-const electron = require("electron");
-const axios = require("axios");
+WindowFunctions = {
+    minimize: function() {
+        window.electronAPI.windowControls('minimize');
+    },
 
-const app = electron.remote.app;
+    maximize: function() {
+        window.electronAPI.windowControls('maximize');
+    },
 
-detect.addEvent
+    close: function() {
+        window.electronAPI.windowControls('close');
+    }
+}
+
+function onDragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    event.currentTarget.style.backgroundColor = 'yellow';
+}
+
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    const id = event.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+
+    const dropzone = event.target;
+    dropzone.appendChild(draggableElement);
+
+    event.dataTransfer.clearData();
+}
--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'js/preload.js'),
+            nodeIntegration: true,
+            enableRemoteModule: true
         }
     });
+
+    ipcMain.on('window-controls', (event, action) => {
+        const webContents = event.sender;
+        const win = BrowserWindow.fromWebContents(webContents);
+        
+        switch(action) {
+            case "minimize":
+                win.minimize();
+                break;
+            case "maximize":
+                if(!win.isMaximized()) {
+                    console.log("Running maximize");
+                    win.maximize();
+                    break;
+                } else {
+                    win.restore();
+                    break;
+                }
+            case "close":
+                win.close();
+        }
+    })
 
     win.loadFile('index.html');
 }
